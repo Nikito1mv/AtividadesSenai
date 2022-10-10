1d9c84b3a3e634b10d38b96a759fb98dde4c5682
Micael Cunha <micaaelvitoor2015@gmail.com>
2022-10-10 02:11:31 -0300

    sla

--- ItemOrcamento/src/itemorcamento/ItemOrcamentoComplexo.java
+++ ItemOrcamento/src/itemorcamento/ItemOrcamentoComplexo.java
@@ -1,8 +1,30 @@
 package itemorcamento;
+
 public class ItemOrcamentoComplexo extends ItemOrcamento{
     
-    public ItemOrcamentoComplexo(String historico, float valor) {
+    private ItemOrcamento[] subItens; 
+    
+    public ItemOrcamentoComplexo(String historico, float valor, ItemOrcamento[] subItens) {
         super(historico, valor);
+        this.subItens = subItens;
+    }
+
+    @Override
+    public float getValor() {
+        float valorTotal = 0;
+        for (ItemOrcamento i :subItens) {
+            valorTotal += i.getValor();
+        }
+        return valorTotal;
+    }
+    
+    public ItemOrcamento encontrarItem(String historico) {
+        for (ItemOrcamento i :subItens) {
+            if(i.getHistorico().equals(historico)){
+                return i;
+            }
+        }
+        return null;
     }
     
 }
